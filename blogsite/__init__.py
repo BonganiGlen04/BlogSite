62b5ef20791f6bb26e977303f858707727306d95
Bongani Maduma <135016371+BonganiGlen04@users.noreply.github.com>
2026-04-01 08:58:18 +0200

    login manager packages

--- blogsite/__init__.py
+++ blogsite/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 from blogsite import routes 
