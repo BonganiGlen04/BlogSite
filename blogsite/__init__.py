1951c42dcd35ee114ffa005049c6b6acb66c687b
Bongani Maduma <135016371+BonganiGlen04@users.noreply.github.com>
2026-04-01 11:41:58 +0200

    implementing authentication

--- blogsite/__init__.py
+++ blogsite/__init__.py
@@ -8,7 +8,9 @@ app.config['SECRET_KEY'] = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login' #redirects to login page if user is not logged in and tries to access a route that requires login
+login_manager.login_message_category = 'info' 
+
+
 from blogsite import routes 
